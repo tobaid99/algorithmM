b67b4aea5c7e5f8284dd8d74e19eec27513e7913
Tareq <tobaid99@gmail.com>
2023-08-02 09:40:59 +0300

    first commit

---
+++ main.js
@@ -0,0 +1,144 @@
+//1-  Print 1 - 135
+// set the start = 0 end - 135
+// foor loop to count
+// print every number
+
+// for( let i=1  ; i<=135; i++){
+//     console.log(i);
+// }
+
+//------------------------------------------------------------------------
+
+
+
+
+// 2- Print Odd Numbers 1 - 135
+// let a = 1 start from 1   and end in 135
+// for loop increment by 2 number
+//  console log a
+
+// for( let  a=1; a <= 135 ; a+=2){
+//     console.log(a);
+// }
+
+
+
+//------------------------------------------------------------------------
+
+// 3- Sum of Printed Numbers between 1 and 135
+//set start with 1  and end with 135
+// let varaible x =0 
+// let y form sum  the number  
+// foor loop 
+// print the output
+
+// let y =0;
+// for(x=0 ; x<= 135 ; x++){
+
+//     y+=x;
+//     console.log(`Number is:${x} Sum: ${y}`);
+
+
+
+//------------------------------------------------------------------------
+
+// 4-  print element of array
+// array is given
+// for loop 
+//print the array
+
+// let z = [1,4,2,12]
+// for(let e=0 ; e<z.length ; e++){
+//     console.log(z[e]);
+// }
+
+//------------------------------------------------------------------------
+
+
+// 5- Find Max
+//let varible = max = 0
+// creat i = 0
+// for loop
+//if i > max
+// max = i
+// print max
+
+
+// let N = [1,4,15,12,0,-3]
+// let max = N[0];
+// ;
+// for (let j= 0 ; j<N.length ; j++){
+//     if( N[j] >max){
+//         max=N[j];
+//     }else{
+
+//     }
+  
+// }
+// console.log(max);
+
+
+
+//------------------------------------------------------------------------
+
+
+// 6. Get Average
+// let x array given
+// let avg =0
+//  foor loop to sum the number of x array
+// print avg= sum / count array length
+
+//  let X = [2,1,3,4,5];
+// let avg= 0;
+// let sum= 0
+
+// for( let i=0 ; i<X.length ; i++){
+//     sum+= X[i];
+   
+// }
+
+// console.log(sum/X.length);
+
+
+
+//------------------------------------------------------------------------
+
+
+// 7. Eliminate the Negatives
+
+// given array
+// for loop 
+// if i < 0
+// i=0
+//print i
+
+
+// let X = [2,-1,4,-3];
+// for(let i=0; i< X.length ; i++){
+//     if(X[i]<0){
+//         X[i]=0;
+      
+//     }else{
+
+//     }
+//     console.log(X[i]);
+// }
+
+
+//------------------------------------------------------------------------
+
+
+//8. Number to String
+
+
+
+let X = [2,-1,4,-3];
+for(let i=0; i< X.length ; i++){
+    if(X[i]<0){
+        X[i]=String;
+
+    }else{
+
+    }
+    console.log(X[i]);
+}
